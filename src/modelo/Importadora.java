64e7d534595c61a6748d64c2fe8cfdc54547ff69
Alessandra <alesantosilva1@gmail.com>
2019-08-07 13:45:32 -0300

    Adicionado os atributos e os metodos getters e setters.

--- src/modelo/Importadora.java
+++ src/modelo/Importadora.java
@@ -1,5 +1,32 @@
 package modelo;
 
 public class Importadora {
+	private String nomeDoResponsavel;
+	private String telefone;
+	private String cgc;
+	
+	public String getNomeDoResponsavel() {
+		return nomeDoResponsavel;
+	}
+	
+	public void setNomeDoResponsavel(String nomeDoResponsavel) {
+		this.nomeDoResponsavel = nomeDoResponsavel;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getCgc() {
+		return cgc;
+	}
+	
+	public void setCgc(String cgc) {
+		this.cgc = cgc;
+	}
 
 }
